eee6ce29764da8bddc2373d844f2a679993d288d
AlexanderPeek <alexanderpeek23@gmail.com>
2024-05-02 21:25:08 -0400

    more progress

--- generateDays.py
+++ generateDays.py
@@ -52,11 +52,23 @@ def generateDays():
             if val < 0:
                 val += 1
 
-            days[j][i] = round(val, 4)
+            days[j][i] = "%.4f" % val
 
             total += 1
 
     return days
+
+
+def getVal(total):
+    val = (total) * 1.0 / 365.24
+
+    if val > 1:
+        val -= int(total / 365) * 1.0
+
+    if val < 0:
+        val += 1
+
+    return "%.4f" % val
 
 
 def generateDaysReverse():
@@ -68,173 +80,53 @@ def generateDaysReverse():
             if j == 2004 and i <= 7:
                 val = i
             else:
-                val = (total) * 1.0 / 365.24
-
-                if val > 1:
-                    val -= int(total / 365) * 1.0
-
-                if val < 0:
-                    val += 1
-
-                val = round(val, 4)
+                val = getVal(total)
             total += 1
             days[j][val] = "Jan " + str(i)
         for i in range(1, 29):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Feb " + str(i)
         for i in range(1, 32):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Mar " + str(i)
         for i in range(1, 31):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Apr " + str(i)
         for i in range(1, 32):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "May " + str(i)
         for i in range(1, 31):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Jun " + str(i)
         for i in range(1, 32):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Jul " + str(i)
         for i in range(1, 32):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Aug " + str(i)
         for i in range(1, 31):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Sep " + str(i)
         for i in range(1, 32):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Oct " + str(i)
         for i in range(1, 31):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Nov " + str(i)
         for i in range(1, 32):
-            val = (total) * 1.0 / 365.24
-
-            if val > 1:
-                val -= int(total / 365) * 1.0
-
-            if val < 0:
-                val += 1
-
-            val = round(val, 4)
+            val = getVal(total)
             total += 1
             days[j][val] = "Dec " + str(i)
-
-        # for i in days[j].keys():
-        #     ex = [
-        #         "Jan 1",
-        #         "Jan 2",
-        #         "Jan 3",
-        #         "Jan 4",
-        #         "Jan 5",
-        #         "Jan 6",
-        #     ]
-        #     if j == 2004 and i in ex:
-        #         continue
-        #     # day = int(i.split(" ")[1])
-        #     val = (total) * 1.0 / 365.24
-
-        #     if val > 1:
-        #         val -= int(total / 365) * 1.0
-
-        #     if val < 0:
-        #         val += 1
-
-        #     days[j][i] = round(val, 4)
-
-        #     total += 1
 
     return days
 
